1d43465def44346b49be8f784113451644244270
William Law <williamlaw.wtl@gmail.com>
2024-07-09 15:02:33 -0700

    panic

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -1896,7 +1896,6 @@ func (s *TransactionAPI) GetRawTransactionByHash(ctx context.Context, hash commo
 // GetTransactionReceipt returns the transaction receipt for the given transaction hash.
 func (s *TransactionAPI) GetTransactionReceipt(ctx context.Context, hash common.Hash) (map[string]interface{}, error) {
 	found, tx, blockHash, blockNumber, index, err := s.b.GetTransaction(ctx, hash)
-	panic("FOUND1")
 	if err != nil {
 		return nil, NewTxIndexingError() // transaction is not fully indexed
 	}
